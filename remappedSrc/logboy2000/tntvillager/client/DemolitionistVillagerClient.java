7b8019dd0e4a8c79be859ffc56ddfe1e526fed81
Logboy2000 <loganhowarth@icloud.com>
2023-10-25 18:45:47 -0600

    Fixed missing recipes, textures, and loot tables

---
+++ remappedSrc/logboy2000/tntvillager/client/DemolitionistVillagerClient.java
@@ -0,0 +1,11 @@
+package logboy2000.tntvillager.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DemolitionistVillagerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
